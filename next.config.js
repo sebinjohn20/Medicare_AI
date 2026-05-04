43b36fe35e5f1ec46f14779e038e4bd697eeb558
unknown <sebinjohn20@gmail.com>
2026-05-04 11:12:29 +0530

    code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
-  images: { remotePatterns: [] },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
